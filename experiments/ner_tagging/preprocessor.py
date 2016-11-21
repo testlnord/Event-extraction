7af5f9f8241ef60930af8f9bf160de9d3601a78d
gkirgizov <gkirgizov@gmail.com>
2016-11-22 01:52:20 +0300

    Small changes in NERPreprocessor to avoid empty sents

--- experiments/ner_tagging/preprocessor.py
+++ experiments/ner_tagging/preprocessor.py
@@ -6,7 +6,10 @@ from experiments.marking.preprocessor import Preprocessor
 class NERPreprocessor:
     def objects(self):
         for sent, tags in chain(self._wikigold_conll(), self._wikiner_wp3()):
-            yield sent, self._check_tags(tags)
+            yield sent, tags
+
+    def __len__(self):
+        return 143840
 
     def _wikigold_conll(self, path='/media/Documents/datasets/ner/wikigold.conll.txt'):
         with open(path) as f:
@@ -14,30 +17,34 @@ class NERPreprocessor:
             tags = []
             for line in f:
                 # print(line.strip())
-                if len(line) > 1:
-                    token, ner_tag = line.strip().split(' ', maxsplit=2)
+                line = line.strip()
+                if len(line) > 0:
+                    token, ner_tag = line.split(' ', maxsplit=2)
                     bio2_tag = ner_tag[0]
                     sent.append(token)
                     tags.append(bio2_tag)
                     assert bio2_tag in ('O', 'I', 'B')
                 else:
-                    yield sent, tags
+                    if len(sent) > 1:
+                        yield sent, self._check_tags(tags)
                     sent.clear()
                     tags.clear()
 
     def _wikiner_wp3(self, path='/media/Documents/datasets/ner/aij-wikiner-en-wp3'):
         with open(path) as f:
             for line in f:
-                sent = []
-                tags = []
-                for token_tag in line.strip().split(' '):
-                    if len(token_tag) > 1:
-                        token, _, ner_tag = token_tag.split('|')
-                        bio2_tag = ner_tag[0]
-                        sent.append(token)
-                        tags.append(bio2_tag)
-                        assert bio2_tag in ('O', 'I', 'B')
-                yield sent, tags
+                line = line.strip()
+                if len(line) > 0:
+                    sent = []
+                    tags = []
+                    for token_tag in line.split(' '):
+                        if len(token_tag) > 0:
+                            token, _, ner_tag = token_tag.split('|')
+                            bio2_tag = ner_tag[0]
+                            sent.append(token)
+                            tags.append(bio2_tag)
+                            assert bio2_tag in ('O', 'I', 'B')
+                    yield sent, self._check_tags(tags)
 
     def _check_tags(self, tags):
         """Change 'I' tags to 'B' tags where necessary (B is for the Beginning of NE)"""
@@ -50,7 +57,9 @@ class NERPreprocessor:
 
 if __name__ == "__main__":
     preprocessor = NERPreprocessor()
-    for sent, tags in preprocessor.objects():
-        print(' '.join(sent))
-        print(' '.join(tags))
+    data = preprocessor.objects()
+    for i, (sent, tags) in enumerate(data):
+        # print(i)
+        print(i, ' '.join(sent))
+        print(i, ' '.join(tags))
         pass
