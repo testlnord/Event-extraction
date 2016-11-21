a4c7edb32cb5963b2e08596522dcfbe984b7c782
gkirgizov <gkirgizov@gmail.com>
2016-11-22 01:46:15 +0300

    Refactor PaddingEncoder to more abstract Padding class, move it to data_common
    And small fixes such as return type in encoder

--- experiments/data_common.py
+++ experiments/data_common.py
@@ -25,7 +25,7 @@ def split_range(length, batch_size, splits):
     return edges
 
 
-def split(slicible, batch_size=1, splits=(0.8, 0.2)):
+def split(slicible, batch_size, splits):
     """Split something on len(splits) parts with sizes proportional values in splits.
     Sizes of the parts will be multiples of batch_size."""
     edges = split(len(slicible), batch_size, splits)
@@ -33,15 +33,57 @@ def split(slicible, batch_size=1, splits=(0.8, 0.2)):
     return subsets
 
 
-def batch(iterable, batch_size=1, element_wise=True):
-    """Yields tuples of batchs (element_wise == True) or batch of tuples (element_wise == False)"""
-    cur_batch = []
-    for elems in iterable:
-        cur_batch.append(elems)
-        if len(cur_batch) == batch_size:
-            # dimension checking
-            # print('batch_shape_before_reshape (expect ({}, {})):'.format(batch_size, len(elems)), np.shape(cur_batch))
-            l = len(elems)
-            yield tuple([np.array([cur_batch[j][i] for j in range(batch_size)]) for i in range(l)]) if element_wise else cur_batch
-            cur_batch = []
+def visualise(model, filename='model.png', exit_after=False):
+    from keras.utils.visualize_util import plot
+    plot(model, show_shapes=True, to_file=filename)
+    log.info('Saved visualisation of the model')
+    if exit_after:
+        exit()
 
+
+class BatchMaker:
+    def __init__(self, batch_size=1, element_wise_batches=True):
+        self.batch_size = batch_size
+        self.element_wise = element_wise_batches
+
+    def __call__(self, iterable):
+        """Yields tuples of batchs (element_wise == True) or batch of tuples (element_wise == False)"""
+        cur_batch = []
+        for elems in iterable:
+            cur_batch.append(tuple(map(np.array, elems)))
+            if len(cur_batch) == self.batch_size:
+                l = len(elems)
+                # transposing if element_wise
+                # in essence: res = zip(*cur_batch); and type conversions
+                res = list(map(np.array, zip(*cur_batch))) if self.element_wise else cur_batch
+                yield res
+                cur_batch.clear()
+
+
+class Padding:
+    def __init__(self, pad_to_length, pad_value=0, pad_tags=False, cut_too_long=True):
+        self.pad_to_length = pad_to_length
+        self.pad_value = pad_value
+        self.pad_tags = pad_tags
+        self.cut_too_long = cut_too_long
+
+    def __call__(self, args_generator):
+        for args in args_generator:
+            yield self.pad(*args[:2])
+
+    def pad(self, text, tag):
+        _text = np.array(text)
+        _tag = np.array(tag)
+
+        ones = len(text)
+        zeros = max(0, self.pad_to_length - ones)
+        pad_text = ((0, zeros), (0, 0))
+        cut = min(ones, self.pad_to_length) if self.cut_too_long else ones
+
+        # mask, telling us about inserted redundant values
+        sample_weights = np.array([1] * cut + [0] * zeros)
+        _text = np.pad(_text[:cut], pad_width=pad_text, mode='constant', constant_values=self.pad_value)
+        if self.pad_tags:
+            _tag = np.pad(_tag[:cut], pad_width=pad_text, mode='constant', constant_values=self.pad_value)
+
+        return _text, _tag, sample_weights
--- experiments/marking/encoder.py
+++ experiments/marking/encoder.py
@@ -1,5 +1,4 @@
 import logging as log
-import numpy as np
 from spacy.tokens import Span, Token
 from experiments.marking.tags import Tags
 
@@ -29,18 +28,15 @@ class Encoder:
 
 
 class SentenceEncoder(Encoder):
-    def encode(self, text: Span, raw_tag) -> (np.ndarray, np.ndarray, np.ndarray):
-        """Encodes single pair of (text: Span, raw_tag).
-        Also returns mask, representing actual values (non-zero) in returned encoded text."""
-        text_encoded, sample_weights = self.encode_text(text)
+    def encode(self, text: Span, raw_tag):
+        text_encoded = self.encode_text(text)
         tag_encoded = self.encode_tag(raw_tag)
-        return np.array(text_encoded), np.array(tag_encoded), np.array(sample_weights)
+        return text_encoded, tag_encoded
 
     def encode_text(self, text: Span):
         # text_encoded = [self._check_and_get_vector(tok) for tok in text]
         text_encoded = [tok.vector for tok in text]
-        sample_weights = [1] * len(text_encoded)
-        return text_encoded, sample_weights
+        return text_encoded
 
     def encode_tag(self, raw_tag):
         return self.tags.encode(raw_tag)
@@ -49,29 +45,3 @@ class SentenceEncoder(Encoder):
         if not (token.like_num or token.is_punct or token.is_space or token.has_vector):
             log.info('Encoder: word "{}" has no embedding (all zeros)!'.format(str(token)))
         return token.vector
-
-
-class PaddingEncoder(SentenceEncoder):
-    def __init__(self, nlp, tags, pad_to_length, pad_value=0, pad_tags=False, none_tag_handler=None):
-        self.pad_to_length = pad_to_length
-        self.pad_value = pad_value
-        self.pad_tags = pad_tags
-        super().__init__(nlp, tags, none_tag_handler)
-
-    def encode(self, text, raw_tag):
-        text_encoded, sample_weights = self.encode_text(text)
-        tag_encoded = self.encode_tag(raw_tag)
-
-        ones = len(text_encoded)
-        zeros = self.pad_to_length - ones
-        pad_text = ((0, zeros), (0, 0))
-
-        # mask, telling us about inserted redundant values
-        sample_weights = np.array([1] * ones + [0] * zeros)
-        text_encoded = np.pad(text_encoded, pad_width=pad_text, mode='constant', constant_values=self.pad_value)
-        if self.pad_tags:
-            tag_encoded = np.pad(tag_encoded, pad_width=pad_text, mode='constant', constant_values=self.pad_value)
-
-        return text_encoded, tag_encoded, sample_weights
-
-
