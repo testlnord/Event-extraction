6ee9ba5a60c78d14f473b312cbbf157bec00d5e6
gkirgizov <gkirgizov@gmail.com>
2016-11-28 12:56:48 +0300

    Improve load_nlp method for loading NERNet

--- experiments/main_pipeline.py
+++ experiments/main_pipeline.py
@@ -4,10 +4,9 @@ import spacy
 from experiments.ner_tagging.net import NERNet
 
 
-# todo: test
 def load_nlp(lang_id='en', path_to_model=None, path_to_vecs=None):
     args = {}
-    if path.isfile(path_to_vecs):
+    if path_to_vecs and path.isfile(path_to_vecs):
         def add_vectors(vocab):
             # vocab.resize_vectors(vocab.load_vectors_from_bin_loc(open(path_to_vecs)))
             vocab.load_vectors_from_bin_loc(path_to_vecs)
@@ -15,22 +14,23 @@ def load_nlp(lang_id='en', path_to_model=None, path_to_vecs=None):
         args['add_vectors'] = add_vectors
         log.info('load_nlp: loading custom word vectors from {}'.format(path_to_vecs))
     else:
-        log.info('load_nlp: file for word vectors not found! path: {}'.format(path_to_vecs))
+        log.info('load_nlp: file for word vectors not found; path: {}'.format(path_to_vecs))
         log.info('load_nlp: loading default word vectors')
 
-    if path.isfile(path_to_model):
+    if path_to_model and path.isfile(path_to_model):
         # todo: think about these paramteres: how to specify them, where to place them
         vector_length = 30000
-        timesteps = 100
-        # predict in fixed batches? make sure last possibly unfull batch is used.
-        batch_size = 32
+        timesteps = 150
+        # predict in fixed batches? make sure last possibly incomplete batch is used.
+        batch_size = 16
 
-        ner_net = NERNet(vector_length, timesteps=timesteps, batch_size=batch_size,
+        ner_net = NERNet(x_len=vector_length, batch_size=batch_size, nbclasses=3, timesteps=timesteps,
                          path_to_model=path_to_model)
         def create_pipeline(nlp):
             return [nlp.tagger, nlp.parser, nlp.entity, ner_net]
+            # return [nlp.tagger, nlp.parser, ner_net]
 
-        args['create_pipeline'] = ner_net
+        args['create_pipeline'] = create_pipeline
         log.info('load_nlp: adding custom entity (iob) tagger to pipeline')
 
     nlp = spacy.load(lang_id, **args)
@@ -41,8 +41,25 @@ def main():
     pass
 
 
+def test_nernet(nlp):
+    with open('samples.txt') as f:
+        log.info('Test NERNet: making doc...')
+        doc = nlp(f.read())
+        log.info('Test NERNet: made doc')
+
+        for n, sent in enumerate(doc.sents):
+            print('\nSENTENCE #{}'.format(n))
+            for i, t in enumerate(sent):
+                print(str(i).rjust(6), t.ent_type_.ljust(10), t.ent_iob_, t)
+
+
 if __name__ == "__main__":
     log.basicConfig(format='%(levelname)s:%(message)s', level=log.DEBUG)
+
     path_to_vecs = '/media/Documents/datasets/word_vecs/glove.840B.300d.bin'
-    path_to_model = 'ner_tagging/model_full.h5'
-    nlp = load_nlp(path_to_model=path_to_model)
+    model_path = 'ner_tagging/models/model_full_epochsize{}_epoch{:02d}_valloss{}.h5'.format(8192, 8, 0.23)
+    nlp = load_nlp(path_to_model=model_path)
+
+    test_nernet(nlp)
+
+
