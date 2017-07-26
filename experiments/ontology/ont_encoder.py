5b724b197f1b24c526e992b0f1ae91fb72a85955
gkirgizov <gkirgizov@gmail.com>
2017-07-26 16:02:37 +0300

    Ontology: encoder: add iob tags as 4th channel: slight improvement on training

--- experiments/ontology/ont_encoder.py
+++ experiments/ontology/ont_encoder.py
@@ -8,7 +8,7 @@ import spacy
 from intervaltree import IntervalTree, Interval
 
 from experiments.tags import CategoricalTags
-from experiments.ontology.symbols import POS_TAGS, DEP_TAGS
+from experiments.ontology.symbols import POS_TAGS, DEP_TAGS, IOB_TAGS, NER_TAGS
 
 
 def is_connected(span):
@@ -118,21 +118,26 @@ def crecord2spans(crecord, nlp):
     return chars2spans(nlp(crecord.context), crecord.s_spanr, crecord.o_spanr)
 
 
+# Reminder: to change the number of chanells, change: self.channels, self.vector_length, self.encode_data
+
 class DBPediaEncoder:
-    def __init__(self, nlp, superclass_map, inverse_map, expand_context=1, augment_data=False):
+    def __init__(self, nlp, superclass_map, inverse_map,
+                 expand_context=1, expand_noun_chunks=False, augment_data=False):
         self.nlp = nlp
         self.classes = superclass_map
         self.inverse_map = inverse_map
         self.tags = CategoricalTags(set(self.classes.values()))
         self.pos_tags = CategoricalTags(POS_TAGS)
         self.dep_tags = CategoricalTags(DEP_TAGS + [''])  # some punctuation marks can have no dependency tag
-        self.channels = 3  # pos_tags, dep_tags, word_vectors
+        self.iob_tags = CategoricalTags(IOB_TAGS)
+        self.channels = 4  # pos_tags, dep_tags, word_vectors
         self._expand_context = expand_context
+        self.expand_noun_chunks = expand_noun_chunks
         self.augment_data = augment_data
 
     @property
     def vector_length(self):
-        return len(self.pos_tags), len(self.dep_tags), self.nlp.vocab.vectors_length
+        return len(self.iob_tags), len(self.pos_tags), len(self.dep_tags), self.nlp.vocab.vectors_length
 
     @property
     def nbclasses(self):
@@ -151,21 +156,23 @@ class DBPediaEncoder:
         :return: encoded data (tuple of arrays)
         """
         sdp = shortest_dep_path(s_span, o_span, include_spans=True, nb_context_tokens=self._expand_context)
-        sdp = expand_ents(sdp)
-        self.last_sdp = sdp  # if there're any need to look at that (for example, for testing)
-        vectors = []
+        sdp = expand_ents(sdp, self.expand_noun_chunks)
+        self.last_sdp = sdp  # for the case of any need to look at that (as example, for testing)
+        _vectors = []
         _pos_tags = []
         _dep_tags = []
+        _iob_tags = []
         for t in sdp:
             log.debug('token: {}; dep_: {}; pos_: {};'.format(t.text, t.dep_, t.pos_))
-            vectors.append(t.vector)
+            _vectors.append(t.vector)
             _pos_tags.append(self.pos_tags.encode(t.pos_))
+            _iob_tags.append(self.iob_tags.encode(t.ent_iob_))
             # _dep_tags.append(dep_tags.encode(t.dep_))
             dep_vars = t.dep_.split('||')  # something very strange!!! need to get the data that makes dep parser do strange things
             dep = self.dep_tags.encode(dep_vars[0])
             # dep = np.sum([np.array(dep_tags.encode(dep_var)) for dep_var in dep_vars])  # incorporate all dep types provided by dep parser...
             _dep_tags.append(dep)
-        return np.array(_pos_tags), np.array(_dep_tags), vectors
+        return np.array(_iob_tags), np.array(_pos_tags), np.array(_dep_tags), _vectors
 
     def encode_class(self, r):
         """
@@ -223,6 +230,6 @@ if __name__ == "__main__":
             if len(sdp) != len(s2):
                 print(s2)
                 print(s3)
-            # for tok, pos, dep, vec in zip(sdp, *data):
+            # for tok, iob, pos, dep, vec in zip(sdp, *data):
             #     print(tok.text.ljust(20), tok.pos_.ljust(10), tok.dep_.ljust(10))
 
