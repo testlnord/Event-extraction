c0213de5f4ffe9fca190e734cc7442e68645fb6d
gkirgizov <gkirgizov@gmail.com>
2017-07-26 14:40:07 +0300

    Ontology: net: experiment: add dropout: slight improvement

--- experiments/ontology/dbpedia_net.py
+++ experiments/ontology/dbpedia_net.py
@@ -3,7 +3,7 @@ from itertools import cycle
 
 import numpy as np
 from keras.models import Model
-from keras.layers import Dense, LSTM, Input, Concatenate, TimeDistributed, Bidirectional, MaxPool1D
+from keras.layers import Dense, LSTM, Input, Concatenate, TimeDistributed, Bidirectional, MaxPool1D, Dropout
 
 from experiments.data_utils import split, visualise
 from experiments.sequencenet import SequenceNet
@@ -14,12 +14,13 @@ class DBPediaNet(SequenceNet):
     def compile1(self):
         xlens = self._encoder.vector_length
 
-        last_units = 256
+        dr = 0.4
+        rdr = 0.4
 
         inputs = [Input(shape=(None, xlen)) for xlen in xlens]
         lstms1 = [LSTM(xlen, return_sequences=True)(input) for input, xlen in zip(inputs, xlens)]
-        lstms2 = [LSTM(xlen, return_sequences=True)(lstm1) for lstm1, xlen in zip(lstms1, xlens)]
-        lstms3 = [LSTM(xlen, return_sequences=False)(lstm2) for lstm2, xlen in zip(lstms2, xlens)]
+        lstms2 = [LSTM(xlen, return_sequences=True, dropout=dr, recurrent_dropout=rdr)(lstm1) for lstm1, xlen in zip(lstms1, xlens)]
+        lstms3 = [LSTM(xlen, return_sequences=False, dropout=dr, recurrent_dropout=rdr)(lstm2) for lstm2, xlen in zip(lstms2, xlens)]
         last = Concatenate(axis=-1)(lstms3)
         dense = Dense(self.nbclasses, activation='softmax')(last)
 
@@ -92,6 +93,8 @@ def eye_test(net, crecords):
 
 
 if __name__ == "__main__":
+    # import spacy
+    # nlp = spacy.load('en')  # it is imported from other files for now
     from experiments.ontology.sub_ont import nlp
     from experiments.ontology.data import props_dir, load_superclass_mapping, load_inverse_mapping, load_all_data
     from experiments.ontology.ont_encoder import crecord2spans
@@ -100,35 +103,34 @@ if __name__ == "__main__":
     # np.random.seed(2)
 
     batch_size = 1
+    prop_case = 'test.balanced0.'
+    model_name = prop_case + 'dr.noaug.v1.'
 
-    # import spacy
-    # nlp = spacy.load('en')  # it is imported from other files for now
-    # scls_file = props_dir + 'prop_classes.csv'
-    # inv_file = props_dir + 'prop_inverse.csv'
-    sclasses = load_superclass_mapping()
-    inverse = load_inverse_mapping()
+    scls_file = props_dir + 'prop_classes.{}csv'.format(prop_case)
+    sclasses = load_superclass_mapping(scls_file)
+    inv_file = props_dir + 'prop_inverse.csv'
+    inverse = load_inverse_mapping(inv_file)
     encoder = DBPediaEncoder(nlp, sclasses, inverse)
     dataset = load_all_data(sclasses, shuffle=True)
     train_data, val_data = split(dataset, splits=(0.8, 0.2), batch_size=batch_size)
     print('total: {}; train: {}; val: {}'.format(len(dataset), len(train_data), len(val_data)))
 
-    epochs = 4
+    epochs = 6
     train_steps = len(train_data) // batch_size
     val_steps = len(val_data) // batch_size
 
     encoder = DBPediaEncoder(nlp, sclasses, inverse, augment_data=False)
-    # net = DBPediaNet(encoder, timesteps=None, batch_size=batch_size)
-    # net.compile1()
-    model_name = 'test_v2_noaug'
-    model_path = 'dbpedianet_model_{}_full_epochsize{}_epoch{:02d}.h5'.format(model_name, train_steps, 2)
-    net = DBPediaNet.from_model_file(encoder, batch_size, model_path=DBPediaNet.relpath('models', model_path))
+    net = DBPediaNet(encoder, timesteps=None, batch_size=batch_size)
+    net.compile1()
+    # model_path = 'dbpedianet_model_{}_full_epochsize{}_epoch{:02d}.h5'.format(model_name, train_steps, 2)
+    # net = DBPediaNet.from_model_file(encoder, batch_size, model_path=DBPediaNet.relpath('models', model_path))
     net._model.summary(line_length=80)
 
-    eye_test(net, val_data)
+    # eye_test(net, val_data)
 
-    # tests = [619, 1034, 1726, 3269, 6990(6992?)]  # some edge cases
-    # net.train(cycle(train_data), epochs, train_steps, cycle(val_data), val_steps, model_prefix=model_name)
+    net.train(cycle(train_data), epochs, train_steps, cycle(val_data), val_steps, model_prefix=model_name)
     # evals = net.evaluate(cycle(val_data), val_steps)
     # print(evals)
+    # tests = [619, 1034, 1726, 3269, 6990(6992?)]  # some edge cases
 
 
