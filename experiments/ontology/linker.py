420a47bd9b3c0916c972af148e5554d3ec482b85
gkirgizov <gkirgizov@gmail.com>
2017-08-15 17:54:29 +0300

    Ontology: EntityLinker: dev in progress. add common update method; save and load methods

--- experiments/ontology/linker.py
+++ experiments/ontology/linker.py
@@ -1,4 +1,6 @@
 import logging as log
+import os
+import pickle
 from collections import defaultdict, namedtuple
 from itertools import permutations
 
@@ -16,37 +18,37 @@ TrieEntry = namedtuple('TrieEntry', ['uri', 'sf', 'ent_type'])
 
 class NERLinker:
     """
-    Disambiguates named entities and stores unknown if they satisfy type restrictions.
+    Disambiguates named entities and stores new unknown if they satisfy type restrictions.
     """
-    # todo: store built model
-    @classmethod
-    def from_storage(cls, path):
-        pass
-
-    def __init__(self, allowed_classes, ner_type_resolver, outer_graph, rdf_namespace=dbo):
-        # self.outer_graph = rdf_dataset.get_context(str(rdf_namespace))
-        self.outer_graph = outer_graph
-        self.ns = rdf_namespace
+    def __init__(self, ner_type_resolver, outer_graph, rdf_namespace=dbo):
         self.ntr = ner_type_resolver
+        # self.outer_graph = rdf_dataset.get_context(str(rdf_namespace))
+        self.outer_graph = outer_graph  # todo: is it needed?
+        self.ns = rdf_namespace
 
         # Init storages
         self._trie = CharTrie()
 
-        _classes = set(allowed_classes)
-        for uri, _, type_uri in gdbo.triples((None, RDFS.type, None)):  # there should not be duplicates
-            # todo: synchronously add to stores each asynchronously generated list of synonyms  (Pool.async_map or like that)
+    def update(self, uri_sf_pairs):
+        uri2sf = groupby(uri_sf_pairs, first)
 
-            ent_type = ner_type_resolver.get_by_type_uri(type_uri)  # todo: ensure no extra work or side effect is done
-            if ent_type in _classes:  # case when ent_type is None is handled
-                base_label = get_label(uri)
-                synonyms = [base_label]
+        # todo: parallelize (try thread-based)
+        uris = list(uri2sf.keys())
+        ent_types = map(self.ntr.get_by_uri, uris)
+
+        for ent_type, ent_uri in zip(ent_types, uris):
+            if ent_type:
+                base_label = get_label(ent_uri)
+                surface_forms = [base_label]
+                surface_forms.extend(uri2sf[ent_uri])
+
                 # -disambigs?
                 # -redirects
                 # -acronyms
-                # todo: ensure synonyms have no None (or otherwise incorrect) entries
+                # todo: ensure surface_forms have no None (or otherwise incorrect) entries
                 # Storing in trie upper-case forms for acronyms and lower-case forms for all other words
-                keys = [syn.lower() if not self._is_acronym(syn) else syn for syn in synonyms]
-                self._trie.update({key: TrieEntry(uri, syn, ent_type) for key, syn in zip(keys, synonyms)})
+                sfkeys = [sf if self._is_acronym(sf) else sf.lower() for sf in surface_forms]
+                self._trie.update({sfkey: TrieEntry(ent_uri, sf, ent_type) for sfkey, sf in zip(sfkeys, surface_forms)})
 
     def _is_acronym(self, text, len_threshold=5):
         return len(text) < len_threshold and text.isupper()
@@ -92,5 +94,44 @@ class NERLinker:
                 if m > metric_threshold:
                     return entry.uri
 
+    # todo:
     def add_ent(self, new_ent):
         pass
+
+    def save(self, model_dir):
+        model_name = type(self).__name__.lower() + '.pck'
+        with open(os.path.join(model_dir, model_name), 'wb') as f:
+            pickle.dump(self._trie, f)
+
+    def load(self, model_dir):
+        model_name = type(self).__name__.lower() + '.pck'
+        with open(os.path.join(model_dir, model_name), 'rb') as f:
+            self._trie = pickle.load(self._trie, f)
+
+
+def from_graph(graph):
+    yield from graph.subjects()
+
+
+def from_crecords(crecords):
+    # any need to filter them?
+    for record in crecords:
+        text = record.context
+        for a, b, ent_uri in record.ents:
+            surface_form = text[a:b]
+            if surface_form:
+                yield ent_uri, surface_form
+
+
+if __name__ == "__main__":
+    log.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=log.INFO)
+
+    from experiments.ontology.sub_ont import NERTypeResolver, gdb
+
+    base_dir = '/home/user/'
+    project_dir = os.path.join(base_dir, 'Event_extraction', 'experiments')
+    model_dir = os.path.join(project_dir, 'models')
+
+    ntr = NERTypeResolver()
+    linker = NERLinker(ner_type_resolver=ntr, outer_graph=gdb)
+    # linker.load(model_dir)
