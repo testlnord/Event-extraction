d967c646f64856749c156e1c662697d13a439269
gkirgizov <gkirgizov@gmail.com>
2017-08-16 17:28:30 +0300

    Ontology: NERLinker: first usable version
    Builds Trie using redirects, disambiguations and in-text links from
    articles. All that is from dbpedia.
    Functionality of adding new entities to the ontology isn't impl-d yet.

--- experiments/ontology/linker.py
+++ experiments/ontology/linker.py
@@ -1,8 +1,12 @@
 import logging as log
 import os
+from math import floor
 import pickle
 from collections import defaultdict, namedtuple
-from itertools import permutations
+from itertools import permutations, chain, zip_longest
+
+from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor
 
 from rdflib.namespace import RDF, RDFS
 from pygtrie import StringTrie, CharTrie
@@ -10,7 +14,8 @@ from cytoolz import groupby, first, second
 from fuzzywuzzy import fuzz
 
 
-from experiments.ontology.sub_ont import get_label, dbo, gdbo
+from experiments.ontology.sub_ont import NERTypeResolver
+from experiments.ontology.sub_ont import get_label, get_fellow_disambiguations, get_fellow_redirects, dbo, gdbo
 
 
 TrieEntry = namedtuple('TrieEntry', ['uri', 'sf', 'ent_type'])
@@ -20,38 +25,62 @@ class NERLinker:
     """
     Disambiguates named entities and stores new unknown if they satisfy type restrictions.
     """
-    def __init__(self, ner_type_resolver, outer_graph, rdf_namespace=dbo):
+    def __init__(self, ner_type_resolver, metric_threshold=0.8):
         self.ntr = ner_type_resolver
-        # self.outer_graph = rdf_dataset.get_context(str(rdf_namespace))
-        self.outer_graph = outer_graph  # todo: is it needed?
-        self.ns = rdf_namespace
 
-        # Init storages
+        # Init storage
         self._trie = CharTrie()
+        self._metric_threshold = metric_threshold
 
     def update(self, uri_sf_pairs):
-        uri2sf = groupby(uri_sf_pairs, first)
-
-        # todo: parallelize (try thread-based)
+        """
+        :param uri_sf_pairs: List[Tuple[URIRef, Option[str]]]: tolerable to None surface forms
+        :return:
+        """
+        uri2sf = groupby(first, uri_sf_pairs)  # group by the same uri
         uris = list(uri2sf.keys())
-        ent_types = map(self.ntr.get_by_uri, uris)
 
-        for ent_type, ent_uri in zip(ent_types, uris):
-            if ent_type:
-                base_label = get_label(ent_uri)
-                surface_forms = [base_label]
-                surface_forms.extend(uri2sf[ent_uri])
+        with Pool() as pool:
+            def mmap(f, it): return list(map(f, it))  # todo: pool.map doesn't work: pickle issues with decorators
+            ent_types = mmap(self.ntr.get_by_uri, uris)
+            labels = mmap(get_label, uris)
+            all_redirects = mmap(get_fellow_redirects, uris)  # lists of synonyms for each uri
+            all_disambigs = mmap(get_fellow_disambiguations, uris)  # lists of omonyms for each uri
 
-                # -disambigs?
-                # -redirects
-                # -acronyms
-                # todo: ensure surface_forms have no None (or otherwise incorrect) entries
-                # Storing in trie upper-case forms for acronyms and lower-case forms for all other words
-                sfkeys = [sf if self._is_acronym(sf) else sf.lower() for sf in surface_forms]
-                self._trie.update({sfkey: TrieEntry(ent_uri, sf, ent_type) for sfkey, sf in zip(sfkeys, surface_forms)})
+            for i, (ent_uri, ent_type, base_label, redirects, disambigs) in enumerate(zip(uris, ent_types, labels, all_redirects, all_disambigs), 1):
+                if ent_type:
+                    entries = {TrieEntry(ent_uri, base_label, ent_type)}  # basic entry
+                    entries.update(TrieEntry(ent_uri, sf, ent_type) for sfs in uri2sf[ent_uri] for sf in sfs)  # entries from provided surface forms
+
+                    redirects_labels = mmap(get_label, redirects)
+                    entries.update(TrieEntry(ent_uri, sf, ent_type) for sf in redirects_labels)
+
+                    disambigs_labels = mmap(get_label, disambigs)
+                    disambigs_types = mmap(self.ntr.get_by_uri, disambigs)
+                    entries.update(TrieEntry(duri, dsf, dtype) for duri, dsf, dtype in zip(disambigs, disambigs_labels, disambigs_types))
+
+                    entries = filter(all, entries)  # all fields of entry should evaluate to True
+                    sfgroups = groupby(lambda entry: entry.sf.lower(), entries)  # build 'index' of trie
+                    _new = _upd = 0
+                    for sfkey, group in sfgroups.items():
+                        if not self._trie.has_key(sfkey):
+                            self._trie[sfkey] = set(group)
+                            _new += 1
+                        else:
+                            self._trie[sfkey].update(group)
+                            _upd += 1
+                    log.info('NERLinker: ent #{}: added {:3d}, updated {:3d} sfgroups; "{}"'.format(i, _new, _upd, str(ent_uri)))
+
 
     def _is_acronym(self, text, len_threshold=5):
         return len(text) < len_threshold and text.isupper()
+
+    def _longest_prefix(self, text):
+        l = len(text)
+        left = max(1, floor(l * self._metric_threshold))
+        for end in range(l, left-1, -1):
+            if self._trie.has_subtrie(text[:end]):
+                return text[:end]
 
     def get_uri(self, span):
         """
@@ -59,25 +88,27 @@ class NERLinker:
         :param span: spacy.token.Span
         :return: URIRef or None
         """
-        text = span.text
-        if not self._is_acronym(text): text = text.lower()  # not-acronyms should be searched by lowe-case form
+        # if self._is_acronym(text): text = span.text  # not-acronyms should be searched by lower-case form
+
+        text = span.text.lower()
         _trie = self._trie
 
         if span.label_ == 'PERSON':
             # If ner type is Person: try all permutations of tokens
             tokens = filter(bool, text.split(' '))
-            lprefixes = [_trie.longest_prefix(' '.join(p))[0] for p in permutations(tokens)]
+            lprefixes = [self._longest_prefix(' '.join(p))[0] for p in permutations(tokens)]
             lprefixes = list(filter(None, lprefixes))
             lprefix = max(lprefixes, key=len, default=None)
         else:
             lprefix, _ = _trie.longest_prefix(text)
 
         if lprefix is not None:
-            candidates = list(_trie.itervalues(prefix=lprefix))
+            candidate_sets = _trie.itervalues(prefix=lprefix)
+            candidates = list(chain.from_iterable(candidate_sets))
             if len(candidates) > 0:
                 return self._fuzzy_search(span, candidates)
 
-    def _fuzzy_search(self, span, candidates, metric=fuzz.ratio, metric_threshold=82):
+    def _fuzzy_search(self, span, candidates, metric=fuzz.ratio):
         """
 
         :param span: spacy.token.Span
@@ -85,13 +116,13 @@ class NERLinker:
         :return: best-matching URIRef or None
         """
         typed = groupby(lambda entry: entry.type == span.label_, candidates)
-        text = span.text
-        # Search using corresponding type first
-        for _candidates in (typed[True], typed[False]):
-            if len(_candidates) > 0:
-                measured = [(metric(entry.text, text), entry) for entry in candidates]
+        # Search with the same type first
+        for is_same_type in [True, False]:
+            _candidates = typed.get(is_same_type)
+            if _candidates:
+                measured = [(metric(entry.sf, span.text), entry) for entry in candidates]
                 m, entry = max(measured, key=second)
-                if m > metric_threshold:
+                if m > self._metric_threshold * 100:
                     return entry.uri
 
     # todo:
@@ -110,28 +141,112 @@ class NERLinker:
 
 
 def from_graph(graph):
-    yield from graph.subjects()
+    yield from zip_longest(set(graph.subjects()), [])  # zip with [None] * len(...)
 
 
 def from_crecords(crecords):
-    # any need to filter them?
     for record in crecords:
-        text = record.context
-        for a, b, ent_uri in record.ents:
-            surface_form = text[a:b]
-            if surface_form:
-                yield ent_uri, surface_form
+        for ent in record.ents:
+            uri = ent.uri
+            surface_form = ent.text
+            if uri and surface_form:
+                surface_form = ent.text.strip(' \n\t,-:;').replace('"', '')
+                surface_form = max(surface_form.split('\n'), key=len)
+                if len(surface_form) > 0:
+                    yield ent.uri, surface_form
 
 
-if __name__ == "__main__":
-    log.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=log.INFO)
+def test_pools():
+    from multiprocessing import Pool
+    from time import process_time, perf_counter
 
-    from experiments.ontology.sub_ont import NERTypeResolver, gdb
+    from experiments.ontology.sub_ont import gf
+
+    ntr = NERTypeResolver()
+    uris = list(gf.subjects(dbo.product))
+    print(len(uris), uris)
+
+    # workers = [64, 128, 256, 512]
+    # for wk in workers:
+    #     tstart = perf_counter()
+    #     with ThreadPoolExecutor(max_workers=wk) as pool:
+    #         ent_types = pool.map(ntr.get_by_uri, uris)
+    #     tend = perf_counter()
+    #     print('elapsed thread-pool({}): {}'.format(wk, tend - tstart))
+
+    workers = [8, 16]
+
+    for wk in workers:
+        tstart = perf_counter()
+        with ProcessPoolExecutor(max_workers=wk) as pool:
+            ent_types = pool.map(ntr.get_by_uri, uris)
+        tend = perf_counter()
+        print('elapsed pool({}): {}'.format(wk, tend - tstart))
+
+
+def main():
+    from experiments.ontology.sub_ont import gf, gfall, dbr
+    from experiments.data_utils import unpickle
 
     base_dir = '/home/user/'
     project_dir = os.path.join(base_dir, 'Event_extraction', 'experiments')
     model_dir = os.path.join(project_dir, 'models')
 
+    dataset_dir = '/home/user/datasets/dbpedia/ner/'
+    dataset_file = 'crecords.v2.pck'
+    crecords = list(unpickle(dataset_dir + dataset_file))
+    data1 = set(from_crecords(crecords))
+    # for i, (ent_uri, sf) in enumerate(data1, 1):
+    #     print(i, str(ent_uri).ljust(80), str(sf))
+    # exit()
+
+    graph = gfall
+    data2 = list(from_graph(graph))
+
     ntr = NERTypeResolver()
-    linker = NERLinker(ner_type_resolver=ntr, outer_graph=gdb)
-    # linker.load(model_dir)
+    linker = NERLinker(ner_type_resolver=ntr)
+    for data in [data1, data2]:
+        linker.update(data)
+        linker.save(model_dir)
+
+    test_linker(linker)
+    linker.load(model_dir)
+    test_linker(linker)
+
+
+def try_trie():
+    from experiments.ontology.sub_ont import gf, gfall, dbr
+
+    ntr = NERTypeResolver()
+    linker = NERLinker(ner_type_resolver=ntr)
+
+    # subjs = list(gfall.subjects(dbr.Microsoft))
+    # subjs = [dbr.Alien, dbr.Microsoft, dbr.JetBrains, dbr.Google]
+    # data = list(zip_longest(subjs, []))
+    data = list(from_graph(gfall))
+
+    print('total entities: {}'.format(len(data)))
+    linker.update(data)
+    print('total entities: {}'.format(len(data)))
+    test_linker(linker)
+
+
+def test_linker(linker):
+    trie = linker._trie
+    entry_sets = list(trie.itervalues())
+    all_entries = set(chain.from_iterable(entry_sets))
+    total_entries = sum(map(len, entry_sets))
+    total_unique_entries = len(all_entries)
+    print('TOTAL KEYS: {}; TOTAL ENTRIES: {} (UNIQUE: {})'.format(len(trie), total_entries, total_unique_entries))
+    assert all(map(all, all_entries)), 'found bad entries: {}'.format(len([e for e in all_entries if not all(e)]))
+    assert total_unique_entries == total_entries
+
+
+if __name__ == "__main__":
+    from experiments.ontology.data_structs import ContextRecord, EntityRecord  # for unpickle()
+    log.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=log.INFO)
+
+    # test_pools()
+    # try_trie()
+
+    main()
